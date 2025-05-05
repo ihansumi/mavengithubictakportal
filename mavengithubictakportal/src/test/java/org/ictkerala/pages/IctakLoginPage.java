411ff44f0e850228a60960ab3b08ad7462dbf8df
SUMI SUNNY <SUMI SUNNY@LAPTOP-JLK0VQOA>
2025-05-05 16:52:35 +0530

    Added more TestCases and Methods and updated

--- mavengithubictakportal/src/test/java/org/ictkerala/pages/IctakLoginPage.java
+++ mavengithubictakportal/src/test/java/org/ictkerala/pages/IctakLoginPage.java
@@ -1,8 +1,16 @@
 package org.ictkerala.pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class IctakLoginPage {
 	
@@ -27,11 +35,114 @@ public class IctakLoginPage {
 	public void btnclklog()
 	{
 		WebElement logbutton=driver.findElement(By.xpath("//button[@type='submit']"));
-		logbutton.click();
+		logbutton.click();	
+	}
+	public String errorMessage()
+	 {
+		WebElement errormsg = driver.findElement(By.xpath("//p[@class='MuiTypography-root MuiTypography-body2 css-1tmw1eq']"));
+		String error=errormsg.getText();
+		return error;
+	 }
+	public void blankusername() {
+		WebElement emailField = driver.findElement(By.name("email"));
+		String validationMessage = (String)((JavascriptExecutor) driver)
+		    .executeScript("return arguments[0].validationMessage;", emailField);
+		System.out.println("Validation Message: " + validationMessage);
+
+	}
+	public void blankpassword() {
+		WebElement passwordField = driver.findElement(By.name("password"));
+		String validationMessage = (String)((JavascriptExecutor) driver)
+		        .executeScript("return arguments[0].validationMessage;", passwordField);
+
+		    System.out.println("Validation Message (blank password): " + validationMessage);
+	}
+	public void blankemailpassword() {
+		 WebElement emailField = driver.findElement(By.name("email"));
+		    WebElement passwordField = driver.findElement(By.name("password"));
+		    String validationMessage = (String)((JavascriptExecutor) driver)
+		            .executeScript("return arguments[0].validationMessage;", emailField);
+
+		        System.out.println("Validation Message (blank email and password): " + validationMessage);
+	}
+	
+	public void addProj() {
+		WebElement addpj=driver.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-colorPrimary MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-colorPrimary css-1hw9j7s']"));
+		addpj.click();
+	}
+	public void pjtopic(String top) {
+		WebElement topic=driver.findElement(By.xpath("//input[@name='topic']"));
+		topic.sendKeys(top);
+	}
+	public void pjduration(String duration) {
+		WebElement dur=driver.findElement(By.xpath("//input[@name='duration']"));
+		dur.sendKeys(duration);
+	}
+	public void butadd() {
+		WebElement add=driver.findElement(By.xpath("//button[text()='Add']"));
+		add.click();
+	}
+	public void blankDurtopic() {
+		try {
+	        Alert alert = driver.switchTo().alert();
+	        String alertText = alert.getText();
+	        System.out.println("Alert Message: " + alertText);
+	        alert.accept();
+	    } catch (NoAlertPresentException e) {
+	        System.out.println("No alert is present.");
+	    }
+	}
+	public void invalidFormats(){
+		try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            System.out.println("Alert Message: " + alertText);
+
+            // Check the message
+            Assert.assertEquals(alertText.trim(), "Please enter a valid topic", "Unexpected alert message.");
+            alert.accept();  // Dismiss the alert
+        } catch (NoAlertPresentException e) {
+            // Fail test if alert is not present
+            Assert.fail("Alert not displayed for invalid credentials(Invalid topic or duration/Invalid topic and duration)");
+        }
+    }
+	public void editmodifybutton() {
+		WebElement editbut=driver.findElement(By.xpath("//button[.//*[name()='svg' and @data-testid='EditIcon']]"));
+		editbut.click();
+	}
+	public void updatebutton() {
+		WebElement updbut=driver.findElement(By.xpath("//button[normalize-space()='Update']"));
+		updbut.click();
+	}
+	public void clearfields() {
+		WebElement topicField = driver.findElement(By.xpath("//input[@name='topic']"));  
+		WebElement durationField = driver.findElement(By.xpath("//input[@name='duration']"));
+
+		// Clear existing content
+		topicField.clear();
+		durationField.clear();	
+	}
+	public void deletebut() {
 		
+		WebElement delete=driver.findElement(By.xpath("//button[normalize-space()='DeleteIcon']"));
+		delete.click();
+
+	}
+	public void okbutton() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement okButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='OK']")));
+		okButton.click();
+
+	}
+	public void logout() {
+		WebElement logoutbutt=driver.findElement(By.xpath("//div[@class='MuiButtonBase-root MuiListItemButton-root MuiListItemButton-gutters MuiListItemButton-root MuiListItemButton-gutters css-16e9raa']"));
+		logoutbutt.click();
+	}
+	public String errorcode() {
+		WebElement err=driver.findElement(By.xpath("//span[@class='error-code']"));
+		String code=err.getText();
+		return code;
 	}
 
+	}
 
-
-
-}
--- mavengithubictakportal/src/test/java/org/ictkerala/testcases/IctakTestcases.java
+++ mavengithubictakportal/src/test/java/org/ictkerala/testcases/IctakTestcases.java
@@ -1,5 +1,7 @@
 package org.ictkerala.testcases;
 
+import static org.testng.Assert.assertEquals;
+
 import org.ictkerala.pages.IctakLoginPage;
 import org.ictkerala.presettings.ConfigurationsPage;
 import org.testng.annotations.BeforeClass;
@@ -8,6 +10,8 @@ import org.testng.annotations.Test;
 public class IctakTestcases extends ConfigurationsPage{
 	
 	IctakLoginPage lobj;
+	String Expected_Message="Invalid username or password";
+	String Expected_msg="404: NOT_FOUND";
 	
 	@BeforeClass
 	public void objinit() {
@@ -18,6 +22,7 @@ public class IctakTestcases extends ConfigurationsPage{
 		lobj.btnclk();
 		
 	}
+	@Test
 	public void TC_2_1_1() {
 		driver.get("https://ictak-internship-portal-client.vercel.app/");
 		lobj.btnclk();
@@ -26,5 +31,241 @@ public class IctakTestcases extends ConfigurationsPage{
 		lobj.btnclklog();
 		
 	}
+	@Test
+	public void TC_2_1_2() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("1111");
+		lobj.btnclklog();
+		String errmsg=lobj.errorMessage();
+		assertEquals(errmsg, Expected_Message);		
+	}
+	@Test
+	public void TC_2_1_3() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("icexample.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		String errmsg=lobj.errorMessage();
+		assertEquals(errmsg, Expected_Message);
+	}
+	@Test
+	public void TC_2_1_4() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.blankusername();
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		
+	}
+	@Test
+	public void TC_2_1_5(){
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.blankpassword();
+		lobj.btnclklog();
+		
+	}
+	@Test
+	public void TC_2_1_6() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.blankemailpassword();
+		lobj.btnclklog();
+	}
+	@Test
+	public void TC_3_1_1() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Testing");
+		lobj.pjduration("6");
+		lobj.butadd();
 
+	}
+	@Test
+	public void TC_3_1_2() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Testing");
+		lobj.pjduration("6 months");
+		lobj.butadd();
+	}
+	@Test
+	public void TC_3_1_3() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Testing");
+		lobj.butadd();
+		lobj.blankDurtopic();	
+	}
+	@Test
+	public void TC_3_1_4() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjduration("6 months");
+		lobj.butadd();
+		lobj.blankDurtopic();
+		
+	}
+	@Test
+	public void TC_3_1_5() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.butadd();
+		lobj.blankDurtopic();
+	}
+	@Test
+	public void TC_3_1_6() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("@#$%%%&**(");
+		lobj.pjduration("6 months+-**///");
+		lobj.butadd();
+		lobj.invalidFormats();
+	}
+	@Test
+	public void TC_3_1_7() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("@#$%%%&**( ");
+		lobj.pjduration("8888");
+		lobj.butadd();
+		lobj.invalidFormats();
+	}
+	@Test
+	public void TC_3_1_8() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("abcdef+*-/*/*/");
+		lobj.pjduration("+*-*/**-++");
+		lobj.butadd();
+		lobj.invalidFormats();
+		
+	}
+	@Test
+	public void TC_3_1_9() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Tester");
+		lobj.pjduration("Months");
+		lobj.butadd();
+		lobj.invalidFormats();
+	}
+    @Test
+	public void TC_3_2_1() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Tester");
+		lobj.pjduration("6 Months");
+		lobj.butadd();
+		lobj.editmodifybutton();
+		lobj.clearfields();
+		lobj.pjtopic("Software Engineer");
+		lobj.pjduration("6");
+		lobj.updatebutton();		
+	}
+    @Test
+    public void TC_3_2_2() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Tester");
+		lobj.pjduration("6 Months");
+		lobj.butadd();
+		lobj.editmodifybutton();
+		lobj.clearfields();
+		lobj.pjtopic("Software Tester");
+		lobj.pjduration("8");
+		lobj.updatebutton();				
+	}
+    @Test
+    public void TC_3_2_3() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("Software Tester");
+		lobj.pjduration("6 Months");
+		lobj.butadd();
+		lobj.editmodifybutton();
+		lobj.clearfields();
+		lobj.pjtopic("Python");
+		lobj.pjduration("4");
+		lobj.updatebutton();	
+		
+	}
+       @Test
+    	public void TC_3_3_1() {
+		driver.get("https://ictak-internship-portal-client.vercel.app/");
+		lobj.btnclk();
+		lobj.getUsername("ictak@example.com");
+		lobj.getPasswd("0000");
+		lobj.btnclklog();
+		lobj.addProj();
+		lobj.pjtopic("COBOL");
+		lobj.pjduration("1");
+		lobj.butadd();
+		lobj.deletebut();
+		lobj.okbutton();
+	}
+       @Test
+       public void TC_4_1_1() {
+   		driver.get("https://ictak-internship-portal-client.vercel.app/");
+   		lobj.btnclk();
+   		lobj.getUsername("ictak@example.com");
+   		lobj.getPasswd("0000");
+   		lobj.btnclklog();
+   		lobj.logout();
+   		driver.get("https://ictak-internship-portal-client.vercel.app/admin");
+   		String error=lobj.errorcode();
+   		assertEquals(error, Expected_msg); 		
+   	}	
 }
